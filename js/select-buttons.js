b2aad0c6e676580cc641ad8d072cd2056d40021c
Anamul10 <anamul.hasan@mnsu.edu>
2022-08-22 16:31:41 -0400

    some vanilla css and bootstrap added

--- js/select-buttons.js
+++ js/select-buttons.js
@@ -1,17 +1,21 @@
+// targetting all the select button by their class name
 let selectedListLength = 0;
 const selectButtons = document.getElementsByClassName('select-btn');
 for (const selectButton of selectButtons) {
+    // adding event listener for each button
     selectButton.addEventListener('click', function (event) {
         const targetButton = event.target;
         const targetPlayerName = targetButton.parentNode.querySelector('h5').innerText;
 
+
         if (selectedListLength >= 5) {
-            alert('You cannot add more than 5 players!');
+            alert('You cannot add more than 5 players!'); //alert for more than 5 players
         }
         else {
-            getPlayerName(targetPlayerName);
-            targetButton.disabled = true;
-            targetButton.classList.add('bg-secondary');
+            getPlayerName(targetPlayerName); //using this function from common.js file
+            targetButton.disabled = true; //disabling button after clicking
+            targetButton.classList.add('bg-secondary'); //changing color for disabled button
+            // getting the length from player list(ol)
             selectedListLength = document.getElementById('selected-players').children.length;
         }
 
