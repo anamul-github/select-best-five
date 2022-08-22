80c45759d69015ace4df508fe98c4c90c672990d
Anamul10 <anamul.hasan@mnsu.edu>
2022-08-22 19:50:15 -0400

    project done

--- js/common.js
+++ js/common.js
@@ -1,3 +1,4 @@
+// this is a common function to get specific players and add them in ordered list
 function getPlayerName(targetPlayerName) {
     const orderedList = document.getElementById('selected-players');
     const newLi = document.createElement('li');
