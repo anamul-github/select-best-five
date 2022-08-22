8bc4121fa9d8a99021b811dfa100ed250377b492
Anamul10 <anamul.hasan@mnsu.edu>
2022-08-22 15:44:03 -0400

    unnecessary file removed and footer added

--- js/budget.js
+++ js/budget.js
@@ -1,37 +1,46 @@
+// adding event listener on calculate button
 document.getElementById('calculate-btn').addEventListener('click', function () {
+    // first getting the input from per player field
     const inputValuePerPlayer = document.getElementById('per-player-field');
     const inputValuePerPlayerString = inputValuePerPlayer.value;
     const inputPerPlayer = parseFloat(inputValuePerPlayerString);
 
+    // then calculating the cost depending on selected players
     const selectedListLength = document.getElementById('selected-players').children.length;
     const costPerPlayer = inputPerPlayer * selectedListLength;
 
-    const playerExpensesValue = document.getElementById('player-expenses');
-    const playerExpensesValueString = playerExpensesValue.innerText;
-    const playerExpenses = parseFloat(playerExpensesValueString);
+    // setting the total player cost on player expenses element
+    const playerExpensesElement = document.getElementById('player-expenses');
+    const playerExpensesElementString = playerExpensesElement.innerText;
+    const playerExpenses = parseFloat(playerExpensesElementString);
 
-    playerExpensesValue.innerText = costPerPlayer;
+    playerExpensesElement.innerText = costPerPlayer;
 })
 
+// adding event listener on calculate total button
 document.getElementById('calculate-total-btn').addEventListener('click', function () {
+    // getting the input from manager field
     const inputValueManager = document.getElementById('manager-field');
     const inputValueManagerString = inputValueManager.value;
     const inputManager = parseFloat(inputValueManagerString);
 
+    // then getting the input from coach field
     const inputValueCoach = document.getElementById('coach-field');
     const inputValueCoachString = inputValueCoach.value;
     const inputCoach = parseFloat(inputValueCoachString);
 
+    // getting the value from player expenses (reusing this code to make it work)
     const playerExpensesValue = document.getElementById('player-expenses');
     const playerExpensesValueString = playerExpensesValue.innerText;
     const playerExpenses = parseFloat(playerExpensesValueString);
 
-
+    // calculating total cost
     const totalCostCalc = playerExpenses + inputManager + inputCoach;
 
-    const totalCostValue = document.getElementById('total-cost');
-    const totalCostValueString = totalCostValue.innerText;
-    const totalCost = parseFloat(totalCostValueString);
+    // setting total cost on Total element
+    const totalCostElement = document.getElementById('total-cost');
+    const totalCostElementString = totalCostElement.innerText;
+    const totalCost = parseFloat(totalCostElementString);
 
-    totalCostValue.innerText = totalCostCalc;
+    totalCostElement.innerText = totalCostCalc;
 })
