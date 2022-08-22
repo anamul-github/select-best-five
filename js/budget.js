f27efb1ea4d1592c0248d172b9ea54114d2edcf8
Anamul10 <anamul.hasan@mnsu.edu>
2022-08-22 15:16:46 -0400

    calculation done

--- js/budget.js
+++ js/budget.js
@@ -7,8 +7,31 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     const costPerPlayer = inputPerPlayer * selectedListLength;
 
     const playerExpensesValue = document.getElementById('player-expenses');
-    const playerExpenses = playerExpensesValue.innerText;
+    const playerExpensesValueString = playerExpensesValue.innerText;
+    const playerExpenses = parseFloat(playerExpensesValueString);
 
     playerExpensesValue.innerText = costPerPlayer;
 })
 
+document.getElementById('calculate-total-btn').addEventListener('click', function () {
+    const inputValueManager = document.getElementById('manager-field');
+    const inputValueManagerString = inputValueManager.value;
+    const inputManager = parseFloat(inputValueManagerString);
+
+    const inputValueCoach = document.getElementById('coach-field');
+    const inputValueCoachString = inputValueCoach.value;
+    const inputCoach = parseFloat(inputValueCoachString);
+
+    const playerExpensesValue = document.getElementById('player-expenses');
+    const playerExpensesValueString = playerExpensesValue.innerText;
+    const playerExpenses = parseFloat(playerExpensesValueString);
+
+
+    const totalCostCalc = playerExpenses + inputManager + inputCoach;
+
+    const totalCostValue = document.getElementById('total-cost');
+    const totalCostValueString = totalCostValue.innerText;
+    const totalCost = parseFloat(totalCostValueString);
+
+    totalCostValue.innerText = totalCostCalc;
+})
